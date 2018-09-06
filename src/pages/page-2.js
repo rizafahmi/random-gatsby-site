832b2758505dfc105c03dceafaf3498ca6474301
Riza Fahmi <rizafahmi@gmail.com>
2018-09-06 09:20:41 +0700

    Finish gatsby site with page 3

---
+++ src/pages/page-2.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const SecondPage = () => (
+  <Layout>
+    <h1>Hi from the second page</h1>
+    <p>Welcome to page 2</p>
+    <Link to="/">Go back to the homepage</Link>
+    <br />
+    <Link to="/page-3">Go to page 3</Link>
+  </Layout>
+)
+
+export default SecondPage
